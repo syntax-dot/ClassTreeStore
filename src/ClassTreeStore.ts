4ed4874e51ce3ef09da11d718517a0033ed3bb20
Aleksandr <75541723+SHKUROLOG@users.noreply.github.com>
2022-11-18 16:34:16 +0300

    remove instance

--- src/ClassTreeStore.ts
+++ src/ClassTreeStore.ts
@@ -66,21 +66,3 @@ export class TreeStore {
   }
 }
 
-const items = [
-    { id: 1, parent: 'root' },
-    { id: 2, parent: 1, type: 'test' },
-    { id: 3, parent: 1, type: 'test' },
-    { id: 4, parent: 2, type: 'test' },
-    { id: 5, parent: 2, type: 'test' },
-    { id: 6, parent: 2, type: 'test' },
-    { id: '7', parent: '8', type: 'test' },
-    { id: '8', parent: 3, type: 'test' },
-    { id: '9', parent: 3, type: 'test' },
-    { id: 7, parent: 4, type: null },
-    { id: 8, parent: 4, type: null },
-]
-
-const ts = new TreeStore(items)
-
-const result = ts.getAllChildrenIds(2)
-console.log('result', result);
